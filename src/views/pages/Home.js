441fb6d3cff18d03f6b547af60cca0f247c822a6
vtscode <ventuscode@gmail.com>
2020-11-09 15:13:51 +0700

    update home dashboard

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import BaseLayout from "../frame/Base";
+import { getAuth } from "redux/reselect";
+import { EllipsisOutlined,FolderOpenTwoTone } from '@ant-design/icons';
+import { Button,Menu, Dropdown, Typography } from "antd";
 
-export default (props) => {
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.rhivent.github.io/">
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.rhivent.github.io/">
+        2nd menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.rhivent.github.io/">
+        3rd menu item
+      </a>
+    </Menu.Item>
+  </Menu>
+);
+
+const DropdownMenu = () => {
+  return (
+    <Dropdown key="more" overlay={menu}>
+      <Button
+        style={{
+          border: 'none',
+          padding: 0,
+        }}
+      >
+        <EllipsisOutlined
+          style={{
+            fontSize: 20,
+            verticalAlign: 'top',
+          }}
+        />
+      </Button>
+    </Dropdown>
+  );
+};
+
+const Home = (props) => {
   const contentProps = {
     breadcrumb : [
       { text : 'Home' },
     ],
-    title : 'Home',
+    title : `Hi ${props.userLogin.user.username.toUpperCase()}, Welcome Home`,
+    extra : {
+      avatar : { src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4' },
+      extra :[
+        <Button key="today" type="text" icon={<FolderOpenTwoTone />}><Typography.Text strong> Today</Typography.Text></Button>,
+        <DropdownMenu key="more" />,
+      ]
+    }
   }
 
   return(
@@ -17,4 +68,10 @@ export default (props) => {
     </div>
   </BaseLayout>
   );
-}
+};
+
+const mapStateToProps = (state) => ({
+  userLogin : getAuth(state),
+});
+
+export default connect(mapStateToProps)(Home);
